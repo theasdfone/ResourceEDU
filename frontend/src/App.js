34e964a49b8580d9e8a225fc48fecf4473be4f1e
theasdfone <63520535+theasdfone@users.noreply.github.com>
2022-08-28 22:18:18 -0400

    expire session when token not valid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,21 +8,28 @@ import AboutPage from './js/views/aboutpage.jsx';
 import ShareList from './js/views/sharelist.jsx';
 import Profile from './js/views/profile.jsx';
 
-import AdminStore from "./js/api/user"
+import AdminStore from "./js/api/user";
 
 export default function App() {
     const AuthenticateUser = ({user, redirect = '/login', children}) => {
-        if (!user) {
-          return <Navigate to={redirect} replace />;
+        if(user) {
+          const expiry = window.atob(user.token.split(".")[1]).exp;
+      
+          if(expiry * 1000 < Date.now()) {
+            return <Navigate to={redirect} replace />;
+          }
+
+          return children
         }
 
-        return children;
+        return <Navigate to={redirect} replace />;
     };
 
     const IsLoggedIn = ({user, redirect = '/dashboard', children}) => {
         if (user) {
           return <Navigate to={redirect} replace />;
-        } 
+        }
+
         return children;
     };
 
