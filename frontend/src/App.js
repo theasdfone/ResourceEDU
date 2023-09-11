eb2c14f4111ae90140ffef48ab7b7562ff38487b
theasdfone <63520535+theasdfone@users.noreply.github.com>
2023-09-10 22:43:54 -0400

    github actions test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 
-// import MainPage from './js/views/mainpage.jsx';
+import MainPage from './js/views/mainpage.jsx';
 import Login from './js/views/login.jsx';
 import Dashboard from './js/views/dashboard';
 import Register from './js/views/register.jsx';
@@ -35,7 +35,7 @@ export default function App() {
 
     return (
         <Routes>
-          {/* <Route path="/" element={<MainPage/>} /> */}
+          <Route path="/" element={<MainPage/>} />
           <Route path="/about" element={<AboutPage/>} />
           <Route path="/register" element={<Register/>} />
           <Route path="/login" element={
