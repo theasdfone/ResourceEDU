ffc05f1cfedceaf70b92f254a1b8f32edcf62a5f
theasdfone <63520535+theasdfone@users.noreply.github.com>
2024-01-26 09:34:12 -0500

    fixes to spring tests

--- backend/guide/src/test/java/com/edu/guide/GuideApplicationTests.java
+++ backend/guide/src/test/java/com/edu/guide/GuideApplicationTests.java
@@ -3,7 +3,7 @@ package com.edu.guide;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GuideApplicationTests.class)
 class GuideApplicationTests {
 
 	@Test
