190ce2d31921291ef913d50620ccca84d7d6caf1
theasdfone <63520535+theasdfone@users.noreply.github.com>
2024-06-10 17:08:06 -0400

    a

--- client/src/environments/environment.js
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    apiUrl: "http://localhost:4000",
-    awsUrl: "https://3o1ysxo0g3.execute-api.us-west-2.amazonaws.com/development/get?key=",
-    clientId: "",
-    clientSecret: "",
-}
